c197602517aae5928c1d7ba9955aec14ea6b3e22
Jason Grewal <jason.grewal@uconn.edu>
2026-02-12 00:21:50 -0500

    updated bot prediction

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,12 +10,19 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
-        random_state=seed
-    )
+    loss="log_loss",
+    learning_rate=0.02,
+    n_estimators=1500,
+    max_depth=3,
+    max_leaf_nodes=18,
+    subsample=0.8,
+    min_samples_leaf=10,
+    min_samples_split=15,
+    max_features="log2",
+    validation_fraction=0.20,
+    n_iter_no_change=30,
+    tol=1e-5,
+    random_state=seed
+)
     model.fit(X, y)
     return model
